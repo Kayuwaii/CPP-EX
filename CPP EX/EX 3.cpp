1d50a72c4344427b3470efbd193b5d012799c9a4
alexcoralt <alexcoralt@1ASIX-PC15->
2017-10-25 13:18:20 +0200

    NANANA

---
+++ CPP EX/EX 3.cpp
@@ -0,0 +1,48 @@
+/**
+* @file EJ 13.cpp
+* @author Alex Coronas Altemir (AKA. Seraphire)
+* @date 16/10/2017
+* @brief C++ Learning Documentation 3.
+*
+* @see https://github.com/Kayuwaii/CPP-EX
+*/
+
+/*
+EXCERCISE 3
+1)
+a-Get name and surname, group(class for example), school and age. [INPUT]
+b-Print the the input and calculate the age in days(aprox.), months(aprox.) and years. [OUTPUT]
+
+2)
+a-Get 2 numbers (num1 y num2). [INPUT]
+b-Print the 5 results of the operations *, + , - , / , %. [OUTPUT]
+
+3) (ADVANCED SOLUTION)
+a- Get the name and price without taxes of a product, the % of taxes and the % of discount.
+b- Print the name, price, price with discount, taxes %, value of taxes, and final price(Format output as a chart). [OUTPUT]
+z- This last part contains "advanced" code (such as classes and loop (Not really THAT advanced, but advanced indeed)).
+*/
+
+//Include the precompiled headers
+#include "stdafx.h"
+
+//Include the "libraries" needed (HEADERS)
+#include <iostream> // Header that defines the standard input/output stream objects
+#include <string>	// This header introduces string types, character traits and a set of converting functions
+#include <vector>	// Header that defines the vector container class. (ArrayList)
+
+
+using namespace std;
+
+//The following region contains the declarations of every method
+#pragma region Method and Class Declaration
+
+
+//We are forward declaring the methods, so they are in scope everywere in the code. 
+#pragma endregion
+
+int main()
+{
+
+	return 0;
+}
